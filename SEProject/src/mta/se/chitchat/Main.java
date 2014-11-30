8ecae5c199b44332c73c818f6c271ae4130be4e7
Vengage <vengage_92@yahoo.com>
2014-11-30 16:49:28 +0200

    Delete Main.java
    Main duplicat

--- SEProject/src/mta/se/chitchat/Main.java
+++
@@ -1,9 +0,0 @@
-package mta.se.chitchat;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("Hello World!");
-	    System.out.println("Program terminated...");
-    }
-}
