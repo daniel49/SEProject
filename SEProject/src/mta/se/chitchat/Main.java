5299c1242a2db236f83e4f8d767ed392c45eeffa
daniel49 <ilie.daniel49@yahoo.com>
2014-11-30 16:40:35 +0200

    Test commit

---
+++ SEProject/src/mta/se/chitchat/Main.java
@@ -0,0 +1,9 @@
+package mta.se.chitchat;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World!");
+	    System.out.println("Program terminated...");
+    }
+}
