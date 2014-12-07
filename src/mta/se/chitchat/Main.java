c9c011d5a3cbe556f652e5354b8a0f362641da3d
daniel49 <ilie.daniel49@yahoo.com>
2014-12-07 19:43:44 +0200

    Implemented voice feature

--- src/mta/se/chitchat/Main.java
+++ src/mta/se/chitchat/Main.java
@@ -1,8 +1,60 @@
 package mta.se.chitchat;
 
+import java.io.ByteArrayOutputStream;
+
+import mta.se.chitchat.Voice;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("ChitChat!");
-    }
+    public static void main(String[] args) throws Exception {
+	    
+    	
+    	final Voice vc = new Voice();
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();	
+
+        Runnable recordRunner = new Runnable() {
+        	 
+    	      public void run() {
+    	    	  try {
+    	    		  vc.recordVoice(out);
+    	    		  System.out.print(out.toByteArray());
+    	    	  }
+    	    	  catch(Exception e) {
+    	    		  System.exit(-1);
+    	    	  }
+    	      }
+          
+          
+        };
+        
+        
+        
+        
+        Thread captureThread = new Thread(recordRunner);
+        captureThread.start();  
+        
+        Thread.sleep(5000);
+        
+
+        final byte[] data = out.toByteArray();
+        
+        Runnable playRunner = new Runnable() {
+        	Voice vc = new Voice();
+    	      public void run() {
+    	    	  try {
+    	    		  vc.playVoice(data);
+    	    	  }
+    	    	  catch(Exception e) {
+    	    		  System.exit(-1);
+    	    	  }
+    		 }
+      
+        };
+        
+        
+        
+        Thread playThread = new Thread(playRunner);
+        playThread.start();
+
+      }
 }
---
+++ src/mta/se/chitchat/Voice.java
@@ -0,0 +1,110 @@
+package mta.se.chitchat;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.FloatControl;
+
+
+public class Voice {
+	
+	private float sampleRate=8000;
+	private int sampleSizeInBits=8;
+	int channels = 1;
+	boolean signed = true;
+	boolean bigEndian = true;
+	
+	
+	
+	public void setAudioFormat(float sampleRate,int sampleSizeInBits,int channels, boolean signed, boolean bigEndian) {
+		
+		this.sampleRate = sampleRate;
+		this.sampleSizeInBits = sampleSizeInBits;
+		this.channels = channels;
+		this.signed = signed;
+		this.bigEndian = bigEndian;
+	
+	}
+	
+	
+	public byte[] recordVoice(ByteArrayOutputStream out) throws Exception {
+		
+			//final ByteArrayOutputStream out = new ByteArrayOutputStream();	    
+		    
+		    final AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed,
+		        bigEndian);
+		    DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+		    final TargetDataLine recLine = (TargetDataLine) AudioSystem.getLine(info);
+		    recLine.open(format);
+		    recLine.start();
+		    
+		    int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+		    byte buffer[] = new byte[bufferSize];
+		    
+	        try {     	
+			      int seconds = 3;
+			      while(seconds>0) {
+			          int count = recLine.read(buffer, 0, buffer.length);
+			          if (count > 0) {
+			            out.write(buffer, 0, count);
+			          }
+			          seconds--;
+			      }  
+		          out.close();
+	        }
+	        catch (IOException e) {
+		          System.err.println("I/O problems: " + e);
+		          System.exit(-1);
+	        } 		    
+		    
+	        return out.toByteArray();
+	}
+	
+	
+	
+	public void playVoice(byte[] audio) throws Exception {
+		
+		InputStream input = new ByteArrayInputStream(audio);	    
+		    
+		final AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed,bigEndian);
+	    
+	    DataLine.Info playInfo = new DataLine.Info(SourceDataLine.class, format);
+	    final SourceDataLine playLine = (SourceDataLine) AudioSystem.getLine(playInfo);  
+	    final AudioInputStream ais = new AudioInputStream(input, format, audio.length
+	        / format.getFrameSize());
+	    playLine.open(format);
+	    
+	    // Increase volume
+	    FloatControl gainControl = (FloatControl) playLine.getControl(FloatControl.Type.MASTER_GAIN);
+	    gainControl.setValue(6.00f);
+	    
+	    playLine.start();
+	    
+	    int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+	    byte buffer[] = new byte[bufferSize];
+		
+	    try {
+	          int count;
+	          while ((count = ais.read(buffer, 0, buffer.length)) != -1) {
+	            if (count > 0) {
+	            	playLine.write(buffer, 0, count);
+	            }
+	          }
+	          playLine.drain();
+	          playLine.close();
+	        }
+	    catch (IOException e) {
+	          System.err.println("I/O problems: " + e);
+	          System.exit(-3);
+	    }
+	}
+}
